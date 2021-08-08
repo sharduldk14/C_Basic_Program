a27db5e4f2611e504d7911db79d02806412cbadb
sharduldk14 <56082215+sharduldk14@users.noreply.github.com>
2021-08-08 17:05:04 +0530

    Add files via upload

---
+++ Sum of two numbers.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    printf("Enter Two Numbers:");
+    scanf("%d %d",&a,&b);
+    printf("The sum of 2 numders is =%d",a+b);
+
+    return 0;
+}
