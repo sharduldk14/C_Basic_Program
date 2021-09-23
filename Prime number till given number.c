d26f29d14128727cbd71bc3e396549fd77ba5cc9
Shardul <56082215+sharduldk14@users.noreply.github.com>
2021-09-23 23:25:56 +0530

    Create Prime number till given number.c
    Prime number
    A prime number is divisible only by 1 and itself. You are given a positive integer. Write an algorithm to find all the prime numbers from 2 to the given positive number.
    
    
    
    Input Format:
    
    The input consists of an integer num, representing the number written on the board.
    
    Output Format:
    
    Print space-separated integers representing the numbers required by the teacher.

---
+++ Prime number till given number.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int mmain ()
+{
+  int num, i, count = 0, j;
+  scanf ("%d", &num);
+  for (i = 2; i < num; i++)
+    {
+      for (j = 1; j <= i; j++)
+	{
+	  if (i % j == 0)
+	    {
+	      count = count + 1;
+	    }
+	}
+      if (count < 3)
+	{
+	  printf ("%d ", i);
+
+	}
+      count = 0;
+
+    }
+
+  return 0;
+}
